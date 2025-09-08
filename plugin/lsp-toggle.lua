1d517b24fd71f31eaee50637d7c573115f923def
Guennadi Maximov C <g.maxc.fox@protonmail.com>
2025-09-08 00:34:18 -0600

    chore: stylua fixes
    Signed-off-by: Guennadi Maximov C <g.maxc.fox@protonmail.com>

--- plugin/lsp-toggle.lua
+++ plugin/lsp-toggle.lua
@@ -1,15 +1,15 @@
 vim.api.nvim_create_user_command('ToggleLSP', function()
-    local commands = require('lsp-toggle.commands')
-    local window = require('lsp-toggle.window')
-    -- toggle the window
-    -- make sure LSP attaches the buffer
-    if commands.is_open then
-        commands.is_open = false
-        commands.unregister_keybinds()
-        window.close_window()
-    else
-        commands.is_open = true
-        commands.register_keybinds()
-        window.open_window()
-    end
+	local commands = require('lsp-toggle.commands')
+	local window = require('lsp-toggle.window')
+	-- toggle the window
+	-- make sure LSP attaches the buffer
+	if commands.is_open then
+		commands.is_open = false
+		commands.unregister_keybinds()
+		window.close_window()
+	else
+		commands.is_open = true
+		commands.register_keybinds()
+		window.open_window()
+	end
 end, { desc = 'Toggle the window to display active lsps' })
